7b590e6e6dd8b3aba1e658fb079bdbb1a96a1424
Luis <hairchevere@gmail.com>
2023-01-16 15:59:38 -0600

    added id & category name syntax

--- develop/models/Category.js
+++ develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Category_name: {
+      type: DataTypes.INTEGER,
+      allowNULL: false
+    },
   },
   {
     sequelize,
