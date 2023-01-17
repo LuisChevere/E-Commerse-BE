7f424bf0cec34966cc093c9c1c8a914ab238cc60
Luis <hairchevere@gmail.com>
2023-01-17 11:26:48 -0600

    created post and put routes

--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('../../config/connection');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -32,16 +33,40 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTagData = await Tag.create(req.body);
+    res.status(200).json(newTagData)
+  } catch (err) {
+    res.status(500).json (err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      where {
+        id: req.params.id,
+      }
+    );
+    if (!updateTagData) {
+      res.status(404).json({ message: 'No tag with said id!'})
+      return;
+    }
+    res.status(200).json('Tag update successful!');
+  } catch (err) {
+    res.status(500).json
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try 
 });
 
 module.exports = router;
